1139ffa83c8d859f9821cf891696d43a8737a9cd
Jérémy <jeremyschneider21@gmail.com>
2020-07-16 22:22:15 +0200

    Update icon

--- content-script.js
+++ content-script.js
@@ -23,13 +23,29 @@ const implementButton = () => {
         const sampled = document.createElement('div');
         sampled.id = 'sampled-track';
 
-        /* Add the button to the player */
+        /* Insert the button to the player */
         const extra = document.querySelector('.ExtraControls');
         extra.prepend(sampled);
 
         /* Add an icon to the button */
-        const icon = document.createElement('img');
-        icon.src = chrome.runtime.getURL('icons/album-18dp.svg');
+        const icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        icon.setAttributeNS(null, 'viewBox', '0 0 24 24');
+        icon.setAttributeNS(null, 'fill', '#b3b3b3');
+        icon.setAttributeNS(null, 'width', '18px');
+        icon.setAttributeNS(null, 'height', '18px');
+
+        const path1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        path1.setAttributeNS(null, 'd', 'M0 0h24v24H0z');
+        path1.setAttributeNS(null, 'fill', 'none');
+        icon.append(path1);
+
+        const path2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        path2.setAttributeNS(
+            null,
+            'd',
+            'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 14.5c-2.49 0-4.5-2.01-4.5-4.5S9.51 7.5 12 7.5s4.5 2.01 4.5 4.5-2.01 4.5-4.5 4.5zm0-5.5c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1z'
+        );
+        icon.append(path2);
 
         sampled.append(icon);
     }
